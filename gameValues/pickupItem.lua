2db52327ab084abb3df1640af446db1b0e47ccfd
Audun Johannes Brandt <audunjoh@ntnu.no>
2016-04-14 17:37:14 +0200

    Updated image names

--- gameValues/pickupItem.lua
+++ gameValues/pickupItem.lua
@@ -4,7 +4,7 @@ _M.itemSizeModifier = 0.06
 _M.goldCoinScale = 0.7
 _M.itemPath = "/images/game_objects/"
 _M.itemExtension = ".png"
-_M.pathGoldCoinImageSheet = "/images/game_objects/goldCoin2.png"
+_M.pathGoldCoinImageSheet = "/images/game_objects/goldCoin.png"
 _M.sequenceGoldCoin = "goldCoin"
 
 
